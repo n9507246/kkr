e9228ab5411566c009f5665e96604b9cabf4da3a
Your Name <you@example.com>
2026-02-17 17:39:35 +0000

    1

--- resources/views/import/form.blade.php
+++ resources/views/import/form.blade.php
@@ -3,31 +3,32 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center mt-4">
-        <div class="col-md-10">
+        <div class="col-md-8">
             <div class="card">
                 <div class="card-header">
                     <h4>Импорт кадастровых номеров из Excel</h4>
                 </div>
 
                 <div class="card-body">
-                    {{-- Информация о поручении --}}
-                    <div class="alert alert-info">
-                        <strong>Поручение:</strong> {{ $order->incoming_number }} 
-                        от {{ $order->incoming_date }}
-                    </div>
-
                     {{-- Инструкция --}}
                     <div class="mb-4">
                         <h5>Инструкция:</h5>
                         <ol>
                             <li>Скачайте шаблон Excel (кнопка ниже)</li>
-                            <li>Заполните файл данными</li>
-                            <li>Сохраните и загрузите файл</li>
+                            <li>Заполните файл:
+                                <ul>
+                                    <li>Данные поручения (номер, даты)</li>
+                                    <li>Кадастровые номера</li>
+                                    <li>Типы объектов и работ</li>
+                                    <li>Исполнителей</li>
+                                </ul>
+                            </li>
+                            <li>Сохраните файл и загрузите его</li>
                         </ol>
                     </div>
 
                     {{-- Форма загрузки --}}
-                    <form action="{{ route('orders.import', $order->id) }}" 
+                    <form action="{{ route('orders.import', $order->id) }}"
                           method="POST" 
                           enctype="multipart/form-data">
                         
@@ -58,25 +59,12 @@
                                 <a href="#" class="btn btn-success me-2">
                                     Скачать шаблон
                                 </a>
-                                <a href="/orders/{{ $order->id }}" class="btn btn-secondary">
+                                <a href="/orders" class="btn btn-secondary">
                                     Отмена
                                 </a>
                             </div>
                         </div>
                     </form>
-
-                    {{-- Блок для ошибок импорта --}}
-                    @if(session('error'))
-                        <div class="alert alert-danger mt-3">
-                            {{ session('error') }}
-                        </div>
-                    @endif
-
-                    @if(session('success'))
-                        <div class="alert alert-success mt-3">
-                            {{ session('success') }}
-                        </div>
-                    @endif
                 </div>
             </div>
         </div>
