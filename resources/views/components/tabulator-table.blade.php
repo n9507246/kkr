f5819abf3ef7cf519dcce4bfaa3eac7bd13923f1
Your Name <you@example.com>
2026-02-24 16:59:44 +0000

    1

--- resources/views/components/tabulator-table.blade.php
+++ resources/views/components/tabulator-table.blade.php
@@ -8,6 +8,33 @@
     'deleteUrl' => null,
     'debug' => false
 ])
+<style>
+    /* Основные стили таблицы */
+    #report-table { border-radius: 8px; overflow: hidden; border: 1px solid #eaecf0; width: 100%; }
+    /* .tabulator-header { text-transform: uppercase; font-size: 0.75rem !important; background-color: #f8f9fa !important; }
+    .tabulator-cell { font-size: 0.85rem !important; vertical-align: middle !important; } */
+
+    /* Стили выпадающего списка колонок */
+    .dropdown-menu { max-height: 500px !important; overflow-y: auto; min-width: 180px !important;  padding: 12px; z-index: 1060; max-height: 250px !important;}
+    .dropdown-item-checkbox { padding: 6px 10px; border-radius: 4px; transition: background 0.2s; cursor: pointer; display: flex; align-items: center; }
+    .dropdown-item-checkbox:hover { background-color: #f8f9fa; }
+    .dropdown-item-checkbox input { cursor: pointer; margin-right: 12px; width: 16px; height: 16px; }
+    .dropdown-item-checkbox label { cursor: pointer; flex: 1; margin: 0; font-size: 0.9rem; user-select: none; }
+
+</style>
+<div class="dropdown">
+    <button class="btn btn-outline-secondary btn-sm dropdown-toggle" type="button" id="columnDropdown" data-bs-toggle="dropdown" data-bs-auto-close="outside">
+        <i class="bi bi-layout-three-columns me-1"></i> Колонки
+    </button>
+    <div class="spisok-polonok dropdown-menu shadow border-0" aria-labelledby="columnDropdown" >
+        <div class="fw-bold small mb-2 border-bottom px-3 py-2">Отображение полей:</div>
+        <div id="columnCheckboxes"></div>
+        <div class="dropdown-divider"></div>
+        <button type="button" class="btn btn-link btn-sm text-decoration-none w-100 text-start" id="resetColumnState">
+            <i class="bi bi-arrow-counterclockwise"></i> Сбросить вид
+        </button>
+    </div>
+</div>
 
 <div style="overflow-x:auto;">
     <div id="{{ $id }}"></div>
@@ -17,20 +44,17 @@
 <script>
 document.addEventListener('DOMContentLoaded', function () {
 
-    const defaultMinWidth = 120;
-    const defaultMaxWidth = 300;
-
     let preparedColumns = @json($columns).map(col => {
         return {
+            widthGrow: 1,
+            minWidth: 120,
             ...col,
-            minWidth: col.minWidth ?? defaultMinWidth,
-            maxWidth: col.maxWidth ?? defaultMaxWidth,
         };
     });
 
 
     const table = new Tabulator("#{{ $id }}", {
-        layout: "fitDataStretch",
+        layout: "fitColumns",
         height: "{{ $height }}",
 
         ajaxURL: "{{ $ajaxUrl }}",
@@ -151,6 +175,42 @@ document.addEventListener('DOMContentLoaded', function () {
     });
     @endif
 
+    table.on("tableBuilt", function() {
+        const container = document.getElementById("columnCheckboxes");
+        if (!container) return;
+        container.innerHTML = "";
+
+        table.getColumns().forEach(column => {
+            const def = column.getDefinition();
+            if (!def.title || def.field === 'id') return;
+
+            const div = document.createElement("div");
+            div.className = "dropdown-item-checkbox";
+            const isVisible = column.isVisible();
+
+            div.innerHTML = `
+                <input type="checkbox" id="check_${def.field.replace(/\./g, '_')}" ${isVisible ? "checked" : ""}>
+                <label for="check_${def.field.replace(/\./g, '_')}">${def.title}</label>
+            `;
+
+            div.querySelector("input").addEventListener("change", function() {
+                if (this.checked) column.show();
+                else column.hide();
+
+                setTimeout(() => table.redraw(true), 10);
+            });
+            container.appendChild(div);
+        });
+    });
+
+
+    // сброс списка отображения колонок
+        document.getElementById("resetColumnState").addEventListener("click", () => {
+            if(confirm("Сбросить все настройки колонок?")) {
+                localStorage.removeItem("tabulator-realEstateTable_vFinal-columns");
+                location.reload();
+            }
+        });
 });
 </script>
 @endpush
