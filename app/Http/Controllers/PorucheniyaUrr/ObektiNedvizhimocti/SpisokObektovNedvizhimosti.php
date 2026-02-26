ddfb23a7da668794541cb393997c7de8ddac6732
Your Name <you@example.com>
2026-02-26 17:54:53 +0000

    баг исправлен

--- app/Http/Controllers/PorucheniyaUrr/ObektiNedvizhimocti/SpisokObektovNedvizhimosti.php
+++ app/Http/Controllers/PorucheniyaUrr/ObektiNedvizhimocti/SpisokObektovNedvizhimosti.php
@@ -13,10 +13,10 @@ class SpisokObektovNedvizhimosti extends Controller
     public function __invoke(Request $request, string $id_poruchenie)
     {
         $spisok_obektov = \App\Models\KadastrovieObekti::query()
-            ->where('id_porucheniya_urr', $id_poruchenie)
+            ->where('poruchenie_id', $id_poruchenie)
             ->orderBy('created_at', 'desc')
             ->get();
-            // dd($spisok_obektov);
+
         return view('porucheniya-urr.obekti-nedvizhimosti.spisok-obektov', [
             "id_poruchenie" => $id_poruchenie,
             "spisok_obektov" => $spisok_obektov
