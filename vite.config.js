bff7de546c4872c08cc82349324d0908bf6bb35c
Your Name <you@example.com>
2026-03-06 10:26:14 +0000

    доступ с хостов в одной сети

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0', // Лучше чем true - слушает все интерфейсы
+        port: 5173,
+        hmr: {
+            host: '192.168.1.237', // Например: '192.168.1.100'
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/guest.js'],
